0a1d9038ba35b150f499c920cce38d87fe758797
pbvhio <alecmaicy@gmail.com>
2023-11-12 12:11:59 +0800

    finalised

--- react-bmi-calculator/src/App.js
+++ react-bmi-calculator/src/App.js
@@ -5,25 +5,38 @@ function App() {
   const [height, setHeight] = useState();
   const [weight, setWeight] = useState(); // values for h
   const [bmi, setBmi] = useState();
+  const [message, setMessage] = useState("");
 
   console.log("height: ", height);
   console.log("weight: ", weight);
 
   return (
     <div className="App">
-      <header className="pe">BMI Calculator</header>
-      <div>
-        Your height:
+      <header className="pe">
+        <text className="text">BMI Calculator</text>
+      </header>
+      <div className="peter">
+        <text className="text">Your height:</text>
         <input type="number" value={height} onChange={handleHeightChange} />
-      </div>
-      <div>
-        Your Weight:
-        <input type="number" value={weight} onChange={handleWeightChange} />
+        <text className="text"> cm</text>
       </div>
 
-      <button className="cal-button" onClick={calculateBMI}>Calculate</button>
+      <div className="peter">
+        <text className="text">Your weight:</text>
+        <input type="number" value={weight} onChange={handleWeightChange} />
+        <text className="text"> kg</text>
+      </div>
+
+      <button className="cal-button" onClick={calculateBMI}>
+        Calculate
+      </button>
+
+      <div className="peter">
+        <text className="text">{`Your BMI is ${bmi ?? ""}`}</text>
+      </div>
+
       <div>
-       {`Your BMI is ${bmi ?? ""}`}
+        <text className="text">{message}</text>
       </div>
     </div>
   );
@@ -32,7 +45,19 @@ function App() {
     // TODO: find the BMI formula.
     // alert("You clicked the button!")
 
-    setBmi(20)
+    var x = weight / ((height / 100) * (height / 100));
+
+    setBmi(x);
+
+    if (x < 18.5) {
+      setMessage("your are underweight");
+    }
+    if (24 > x && x >= 18.5) {
+      setMessage("your are normal");
+    }
+    if (x >= 24) {
+      setMessage("your are overweight");
+    }
   }
 
   function handleHeightChange(event) {
